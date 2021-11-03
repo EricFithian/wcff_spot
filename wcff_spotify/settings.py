5d6d239b19e92550efd76338dc0baf6a3f433cde
Eric Fithian <ef2413@columbia.edu>
2021-11-03 16:44:55 -0700

    Debugging is fun

--- wcff_spotify/settings.py
+++ wcff_spotify/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-c-o*r!fvh$q926s@kg9t1$frubx%99j=a7y(v^by4u4-)1@b1g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
